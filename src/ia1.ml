d4145332c52b3df28dac160c4ead5d46375f8ca5
Jacques-Henri Jourdan <jacques-henri.jourdan@inria.fr>
2015-08-08 12:20:40 +0200

    revert adapt depth

--- src/ia1.ml
+++ src/ia1.ml
@@ -12,7 +12,8 @@ let find_reachable_states init =
     let seen = Rules.HashConfig.create 17 in
     let todo = Queue.create () in
     Queue.push (init, []) todo;
-    let ends = ref [] in
+    let next = Rules.HashConfig.create 17 in
+    let best_ends = ref (-1, []) in
     while not (Queue.is_empty todo) do
       let (conf, path) = Queue.pop todo in
       if Rules.HashConfig.mem seen conf then ()
@@ -41,16 +42,22 @@ let find_reachable_states init =
           begin match !lock_action with
           | None -> ()
           | Some act ->
-            let conf =
-              try Cont (play_action conf act)
-              with Rules.End (score, _path) -> End (score)
-            in
-            ends := (conf, List.rev (act::path))::!ends end
+            try
+              Rules.HashConfig.replace next (play_action conf act)
+                (List.rev (act::path))
+            with Rules.End (score, _path) ->
+              if score > fst !best_ends then
+                best_ends := (score, List.rev (act::path))
+          end
         end
     done;
-    assert (!ends <> []);
-    Rules.HashConfig.replace find_reachable_states_mem init !ends;
-    !ends
+    let next = Rules.HashConfig.fold (fun conf path acc -> (Cont conf,path)::acc) next [] in
+    let next =
+      if fst !best_ends >= 0 then (End (fst !best_ends), snd !best_ends)::next else next
+    in
+    assert (next <> []);
+    Rules.HashConfig.replace find_reachable_states_mem init next;
+    next
 
 let heuristic_score conf =
   match conf with
@@ -58,53 +65,40 @@ let heuristic_score conf =
   | Cont conf ->
     let sc = ref (conf.score*10000) in
     Bitv.iteri_true (fun bit ->
-      sc := !sc + snd (Rules.coord_of_bit conf bit))
+      let h = Rules.height conf - snd (Rules.coord_of_bit conf bit) in
+      sc := !sc - h*h)
       conf.full_cells;
     !sc
 
-exception TooMuchColl
-let best_heuristic_score_hsize = 10007
-let best_heuristic_score_mem = Array.make best_heuristic_score_hsize None
-let n_coll = ref 0
+let best_heuristic_score_mem =
+  Rules.HashConfig.create 17
 let rec best_heuristic_score conf = function
   | 0 -> heuristic_score conf
   | depth ->
     match conf with
     | End _ -> heuristic_score conf
     | Cont conf ->
-      let h = Rules.HashableConfig.hash conf mod best_heuristic_score_hsize in
-      match best_heuristic_score_mem.(h) with
-      | Some (conf', sc) when Rules.HashableConfig.equal conf conf' -> sc
-      | bucket ->
-        if bucket <> None then
-          begin
-            incr n_coll;
-            if !n_coll > 100 then raise TooMuchColl
-          end;
+      try
+        Rules.HashConfig.find best_heuristic_score_mem conf
+      with Not_found ->
         let next = find_reachable_states conf in
         let res =
           List.fold_left (fun acc (conf, _) ->
             let score = best_heuristic_score conf (depth-1) in
             max acc score) min_int next
         in
-        best_heuristic_score_mem.(h) <- Some (conf, res);
+        Rules.HashConfig.replace best_heuristic_score_mem conf res;
         res
 
 let rec play conf =
-  HashConfig.clear find_reachable_states_mem;
   let next = find_reachable_states conf in
-  let conf_end = ref conf in
-  begin try
-    for depth = 0 to 10 do
-      Array.fill best_heuristic_score_mem 0 best_heuristic_score_hsize None;
-      let (_, path) =
-        List.fold_left (fun ((scoremax, pathmax) as acc) (conf,path) ->
-          let score = best_heuristic_score conf depth in
-          if score <= scoremax then acc
-          else (score, path)
-        ) (min_int, []) next
-      in
-      conf_end := List.fold_left play_action conf path
-    done
-  with TooMuchColl -> () end;
-  !conf_end
+  let (_, path) =
+    List.fold_left (fun ((scoremax, pathmax) as acc) (conf,path) ->
+      let score = best_heuristic_score conf 4 in
+      if score <= scoremax then acc
+      else (score, path)
+    ) (min_int, []) next
+  in
+  HashConfig.clear best_heuristic_score_mem;
+  HashConfig.clear find_reachable_states_mem;
+  List.fold_left play_action conf path
