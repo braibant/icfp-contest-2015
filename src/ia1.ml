180e743450f1fe70a92c772b4b902095bf833b03
Jacques-Henri Jourdan <jacques-henri.jourdan@inria.fr>
2015-08-08 12:21:09 +0200

    depth=0

--- src/ia1.ml
+++ src/ia1.ml
@@ -94,7 +94,7 @@ let rec play conf =
   let next = find_reachable_states conf in
   let (_, path) =
     List.fold_left (fun ((scoremax, pathmax) as acc) (conf,path) ->
-      let score = best_heuristic_score conf 4 in
+      let score = best_heuristic_score conf 0 in
       if score <= scoremax then acc
       else (score, path)
     ) (min_int, []) next
