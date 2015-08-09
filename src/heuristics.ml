da1d8b18d0b141130a7b0c26fbb195b5cb0260fd
Thomas <thomas@cryptosense.com>
2015-08-09 16:50:34 +0200

    Ideas of heuristics

--- src/heuristics.ml
+++ src/heuristics.ml
@@ -17,16 +17,20 @@ let delta = [(0, 1);(1, 0);(0, -1);(-1, 0);(1,-1);(-1,1)]
 let neigh cell =  List.map (Cell.(+) cell) delta
 let heuristic_neighbours data config =
   let sc = ref 0 in
+  let n = ref 0 in
   Bitv.iteri_true (fun bit ->
+      n := 0;                   (* number of full neighbours *)
       let cell = cell_of_bit data bit in
       let neigh = neigh cell in
-      let neigh = List.filter (fun c -> check_cell data c = 0) neigh in
-      let neig_empty = List.filter (fun c ->
-          not (Bitv.get config.full_cells (bit_of_cell data c)))
-          neigh
-      in
-      if List.length neig_empty <= 1 then sc := !sc - 40;
-      if List.length neig_empty = 2 then sc := !sc - 10;
+      List.iter (fun x ->
+          if check_cell data x = 0
+          then
+            if Bitv.get config.full_cells (bit_of_cell data x)
+            then incr n
+            else ()
+          else incr n          (* if the cell is not valid, count is a a neighbour *)
+        ) neigh;
+      if 5 <= !n  then sc := !sc - 10;
     ) (Bitv.bw_not config.full_cells);
   !sc
 
@@ -41,7 +45,7 @@ let heuristic_base data config =
   !sc
 
 let score data config =
-  config.score * 10_000
+  config.score  * 10_000
 
 let signals =
   [|
@@ -51,8 +55,47 @@ let signals =
     score, "score"
   |]
 
+
+(* https://codemyroad.wordpress.com/2013/04/14/tetris-ai-the-near-perfect-player/
+
+   the bumpiness measure the absolute value of the difference between
+   the number of elements in each column. We want to minimize this
+   value (hence, the score is negative)
+
+*)
+let heuristic_bumpiness data config =
+  (* compute the number of elements in  *)
+  let sc = ref 0 in
+  let width = Rules.width data in
+  let table = Array.make width 0 in
+  (* let height = Rules.height data in *)
+  Bitv.iteri_true (fun bit ->
+      let col = fst (Rules.coord_of_bit data bit) in
+      table.(col) <- table.(col) + 1;
+    )
+    config.full_cells;
+  for i = 1 to width - 1 do
+    sc := !sc - abs (table.(i) - table.(i - 1));
+  done;
+  !sc
+
 let simple data config =
-  (* heuristic_base data config  +  *) score data config + heuristic_line data config
+  (* 0 *)
+  + heuristic_base data config
+  + score data config +
+  + heuristic_line data config
+  (* + heuristic_neighbours data config *)
+  (* + heuristic_bumpiness data config *)
+
+(* let medium data config = *)
+(*   score data config *)
+(*   + heuristic_line data config *)
+(*   + heuristic_neighbours data config *)
+
+(* http://www.cs.cmu.edu/afs/cs/project/ACRL/www/TetrisReports/Breelyn_Eric_Don_Project.pdf *)
+
+
+
 
 let meta weights data config : int =
   let acc = ref 0. in
