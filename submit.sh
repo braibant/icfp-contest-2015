251eb8a595fc38bd43d08de60532b0be86576453
Jacques-Henri Jourdan <jacques-henri.jourdan@inria.fr>
2015-08-09 13:20:01 +0200

    submit.sh

--- submit.sh
+++ submit.sh
@@ -1,7 +1,6 @@
 API_TOKEN="c0x8nEeXAxmBWDULUUsj0yXRzhbwaWvGaSjlISKxmok="
 TEAM_ID=36
-OUTPUT=$1
 
 curl --user :$API_TOKEN -X POST -H "Content-Type: application/json" \
-        -d '$OUTPUT' \
+        -d @"$1" \
         https://davar.icfpcontest.org/teams/$TEAM_ID/solutions
