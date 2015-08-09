4ea1776250e149577e9cc3a53f181c9cdc454d53
Jacques-Henri Jourdan <jacques-henri.jourdan@inria.fr>
2015-08-09 21:31:53 +0200

    submit2

---
+++ submit2.sh
@@ -0,0 +1,3 @@
+curl --user :+KSiDjDgSn30VGHI224tMmzvwIitKgrCDgRAlFCS5L4= -X POST -H "Content-Type: application/json" \
+        -d @"$1" \
+        https://davar.icfpcontest.org/teams/278/solutions
